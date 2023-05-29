5fda01715d5a9c11a4dd09c15d632cff2d7d102d
Akash A <akash.a.dckap@gmail.com>
2023-05-29 15:59:19 +0530

    Palindrome Sum (Checking given value is same or not after reversing)

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//Declaring function
+function palindrome($input){
+    //Declaring variable to store reverse string 
+    $reverse = "";
+    //Reverse loop
+    for($i=strlen($input)-1; $i>=0; $i--){
+        //Assaing value to reverse variable 
+       $reverse = $reverse.$input[$i];
+    }
+    //Checking the value is palindrome or not
+    if($reverse == $input){
+        return $input." "."It is a Palindrome";
+    }
+    else{
+        return $input." "."It is not a Palindrome";
+    }
+}
+echo palindrome("malayala")
+
+?>
