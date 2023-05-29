aec8c4748c734e187506885134b7ddccf106b9cc
Akash A <akash.a.dckap@gmail.com>
2023-05-29 16:31:28 +0530

    Numbers also check

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 //Declaring function
 function palindrome($input){
-    //Declaring variable to store reverse string 
+    //Declaring variable to store reverse string
+    $input = strval($input); 
     $reverse = "";
     //Reverse loop
+    
     for($i=strlen($input)-1; $i>=0; $i--){
         //Assaing value to reverse variable 
        $reverse = $reverse.$input[$i];
@@ -17,6 +19,6 @@ function palindrome($input){
         return $input." "."It is not a Palindrome";
     }
 }
-echo palindrome("malayala")
+echo palindrome("malayalam");
 
 ?>
